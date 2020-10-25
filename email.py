ac580e1eeab27af9b94f5cb9293212f56b7ca853
swansosj <sheldon.j.swanson@gmail.com>
2020-10-25 17:17:07 -0400

    Revert "Added comments to code"

--- email.py
+++ email.py
@@ -8,19 +8,16 @@ a list and prints the list.
 
 addresses = []
 
-#asks user if they have an email address to enter, user must input yes or no
 more = input("Do you have an email address to enter (y/n)? ")
 
-#asks users to for email address to enter
 while more == "y":
     email = input("Enter the address: ")
     addresses.append(email)
-#Asks user to input another address
     more = input("Do you have another address(y/n)? ")
     while more != "y":
         if more == "n":
             break
         else:
             more = input("Please enter a y or n: ")
-#prints email addresses    
+    
 print(addresses)
